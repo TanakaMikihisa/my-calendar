f952d033262720a4dc400b9093e1fdb3bbe86ba9
tanakamikihisa <minemiki0901@gmail.com>
2026-03-12 16:15:52 +0900

    Initial Commit

---
+++ MyCalender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyCalender
+//
+//  Created by tanakamiki on 2026/03/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyCalender/MyCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalenderApp.swift
+//  MyCalender
+//
+//  Created by tanakamiki on 2026/03/12.
+//
+
+import SwiftUI
+
+@main
+struct MyCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
