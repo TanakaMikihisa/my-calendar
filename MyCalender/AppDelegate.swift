ea8b58a2c34d151a84cdfc1c2f19f4d978a21e53
tanakamikihisa <minemiki0901@gmail.com>
2026-03-19 20:55:59 +0900

    feat: firebase App Checkの実装

--- MyCalender/AppDelegate.swift
+++ MyCalender/AppDelegate.swift
@@ -1,11 +1,28 @@
 import UIKit
 import FirebaseCore
+import FirebaseAppCheck
+
+final class MyAppCheckProviderFactory: NSObject, AppCheckProviderFactory {
+    func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
+        #if targetEnvironment(simulator)
+        return AppCheckDebugProvider(app: app)
+        #else
+        if #available(iOS 14.0, *) {
+            return AppAttestProvider(app: app)
+        } else {
+            return DeviceCheckProvider(app: app)
+        }
+        #endif
+    }
+}
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
+        let providerFactory = MyAppCheckProviderFactory()
+        AppCheck.setAppCheckProviderFactory(providerFactory)
         FirebaseApp.configure()
         return true
     }
